4e4a0c61ca01ca029ce8e132ca6bde4fe7ea07a1
En Dru Tortoise <En.Dru@westen.de>
2022-05-03 15:38:08 +0200

    Command
    Signed-off-by: En Dru Tortoise <En.Dru@westen.de>

---
+++ HeadFirstPattern.Singleton/ChocolateBoiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstPattern.Singleton
+{
+    internal class ChocolateBoiler
+    {
+        public bool IsEmpty { get; set; }
+        public bool IsBoiled { get; set; }
+        public Guid MyGuid { get; set; }= Guid.NewGuid();
+        private static ChocolateBoiler _instance;
+
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (_instance==null)
+                {
+                    _instance = new ChocolateBoiler();
+                }
+                return _instance; }
+        }
+
+        private ChocolateBoiler() { }
+        public void Fill()
+        {
+            if (IsEmpty)
+            {
+                IsEmpty = false;
+                IsBoiled = false;
+            }
+        }
+        public void Drain()
+        {
+            if (!IsEmpty&&IsBoiled)
+            {
+                IsEmpty = true;
+            }
+        }
+        public void Boil()
+        {
+            if (!IsEmpty && !IsBoiled)
+            {
+                IsBoiled = true;
+            }
+        }
+    }
+}
