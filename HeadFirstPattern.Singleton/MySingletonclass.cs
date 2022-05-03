ec1c06ffc6ef184d8a9834e11ae58e7346f50f6b
En Dru Tortoise <En.Dru@westen.de>
2022-05-03 15:38:08 +0200

    Singleton
    Signed-off-by: En Dru Tortoise <En.Dru@westen.de>

---
+++ HeadFirstPattern.Singleton/MySingletonclass.cs
@@ -0,0 +1,26 @@
+﻿namespace HeadFirstPattern.Singleton
+{
+    internal class MySingletonclass
+    {
+        private static MySingletonclass _instance;
+
+        public Guid MyGuid { get; set; }
+        MySingletonclass()
+        {
+            MyGuid = Guid.NewGuid();
+            
+        }
+        public static MySingletonclass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance=new MySingletonclass();
+                }
+                return _instance;
+            }
+           
+        }
+    }
+}
