d9fc656d0f722925f326cda5fd07021ee1569d12
En Dru Tortoise <En.Dru@westen.de>
2022-05-09 12:46:58 +0200

    GarageCommand added

---
+++ HeadFirstPattern.Command/FremdApis/GarageDoor.cs
@@ -0,0 +1,22 @@
+﻿namespace HeadFirstPattern.Command.Commands
+{
+    public class GarageDoor
+    {
+        public void Up()
+        {
+            Console.WriteLine("Tor ist aufgefahren!");
+        }public void Down()
+        {
+            Console.WriteLine("Tor ist runtergefahren!");
+        }public void Stop()
+        {
+            Console.WriteLine("Tor ist gestoppt!");
+        }public void LightOn()
+        {
+            Console.WriteLine("Licht in Garage ist an!");
+        }public void LightOff()
+        {
+            Console.WriteLine("Licht in Garage ist aus!");
+        }
+    }
+}
--- HeadFirstPattern.Command/Program.cs
+++ HeadFirstPattern.Command/Program.cs
@@ -4,7 +4,11 @@ using HeadFirstPattern.Command.Commands;
 using HeadFirstPattern.Command.FremdApis;
 
 var light = new Light();
+var garage = new GarageDoor();
 var lightCommand = new LightOnCommand(light);
+var garageCommand = new GarageDoorOpenCommand(garage);
 var remoteControl = new SimpleRemoteControl(lightCommand);
 
+remoteControl.ButtonWasPressed();
+remoteControl.SetSlot(garageCommand);
 remoteControl.ButtonWasPressed();
